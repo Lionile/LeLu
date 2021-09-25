68c6d92ca436c1ce02317687e07678fd81cc9a74
Lionile <leon.maderic@gmail.com>
2021-09-25 07:47:35 +0200

    Added function for fuel button.

--- app/src/main/java/com/example/lelu/MainActivity.java
+++ app/src/main/java/com/example/lelu/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lelu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void BtnOnClkFuel()
+    {
+        Intent intent = new Intent(this, FuelActivity.class);
+        startActivity(intent);
+    }
+
 }
