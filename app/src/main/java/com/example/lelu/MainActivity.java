1b35e4afd418a03e5c58763c8198820f5694a5b1
Lionile <leon.maderic@gmail.com>
2021-09-25 16:56:40 +0200

    Added aditional elements to fuel activity.

--- app/src/main/java/com/example/lelu/MainActivity.java
+++ app/src/main/java/com/example/lelu/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void BtnOnClkFuel()
+    public void BtnOnClkFuel(View v)
     {
         Intent intent = new Intent(this, FuelActivity.class);
         startActivity(intent);
